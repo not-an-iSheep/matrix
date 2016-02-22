bac4e418792796ff5b8ba70a6b02e6500b037909
ndemon <joshuapritsker@gmail.com>
2016-02-21 16:36:26 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ ! -d "/usr/share/cowsay/" ]; then
+echo "ERROR: cowsay not installed!
+echo "attempting installation..."
+sleep 3
+sudo apt-get install cowsay
+if [ $? -eq 0 ]; then
+    echo "OK!"
+    sudo cp matrix.cow /usr/share/cowsay/cows
+    cp matrix /usr/bin
+    echo "Done! now you can run matrix"
+    echo "TIP: add matrix to your bashrc!"
+else
+    echo "ERROR: Cowsay install failed, please install cowsay and rerun this script."
+fi
+fi
